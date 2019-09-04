8e843c8c486527ea076efe847a6f87d723fd4207
Ben Jackson <b@benjackson.email>
2019-09-04 21:37:24 +1000

    add more project commands with auto-token renew

--- cmd/projectupdate.go
+++ cmd/projectupdate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/cdchris12/lagoon-cli/graphql"
 	"github.com/spf13/cobra"
 )
 
@@ -18,6 +19,16 @@ var projectUpdateCmd = &cobra.Command{
 		projectName := args[0]
 		projectProperty := args[1]
 		projectValue := args[2]
+
+		// get a new token if the current one is invalid
+		valid := graphql.VerifyTokenExpiry()
+		if valid == false {
+			loginErr := loginToken()
+			if loginErr != nil {
+				panic(loginErr)
+			}
+		}
+
 		fmt.Println(fmt.Sprintf("Updating %s property %s with %s", projectName, projectProperty, projectValue))
 	},
 }
